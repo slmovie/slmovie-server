134416e96e096dc46f3abf0f1c930c57edabf068
baojun <bj805887485@aliyun.com>
2017-03-07 22:11:19 +0800

    主页电影推荐完成

--- bin/app.js
+++ bin/app.js
@@ -5,14 +5,14 @@ let search = require('../routes/search.js')
 let detail = require('../routes/detail.js')
 let index = require('../routes/index.js')
 
-app.all('*', function (req, res, next) {
-    res.set({
-        'Access-Control-Allow-origin': '*',
-        'Access-Control-Allow-Headers': 'X-Requested-With',
-        'Access-Control-Allow-Methods': 'GET'
-    });
-    next();
-});
+// app.all('*', function (req, res, next) {
+//     res.set({
+//         'Access-Control-Allow-origin': '*',
+//         'Access-Control-Allow-Headers': 'X-Requested-With',
+//         'Access-Control-Allow-Methods': 'GET'
+//     });
+//     next();
+// });
 
 app.use(express.static('web/app'));
 
