96f8006f5d9b4b81e425a5c3cfadc744ae48be0b
baojun <bj805887485@aliyun.com>
2017-03-10 21:49:07 +0800

    主线版本确认

--- web/app/reactjs/Service/Constans.js
+++ web/app/reactjs/Service/Constans.js
@@ -2,6 +2,6 @@
  * Created by BaoJun on 2017/2/23.
  */
 //测试地址T，生产地址P
-exports.service = 'T'
+exports.service = 'P'
 exports.log = true
 exports.error = true
--- web/public/build/detail.bundle.js
+++ web/public/build/detail.bundle.js
@@ -32041,7 +32041,7 @@ module.exports = traverseAllChildren;
  * Created by BaoJun on 2017/2/23.
  */
 //测试地址T，生产地址P
-exports.service = 'T';
+exports.service = 'P';
 exports.log = true;
 exports.error = true;
 
