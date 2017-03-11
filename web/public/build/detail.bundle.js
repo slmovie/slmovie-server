627906dea235af42aa91e879c58d2a5c00f8e418
baojun <bj805887485@aliyun.com>
2017-03-11 21:47:38 +0800

    生产配置

--- web/public/build/detail.bundle.js
+++ web/public/build/detail.bundle.js
@@ -32046,9 +32046,9 @@ module.exports = traverseAllChildren;
  * Created by BaoJun on 2017/2/23.
  */
 //测试地址T，生产地址P
-exports.service = 'T';
-exports.log = true;
-exports.error = true;
+exports.service = 'P';
+exports.log = false;
+exports.error = false;
 
 /***/ }),
 /* 182 */,
