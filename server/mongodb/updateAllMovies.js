c87b300c3d678f30b4b88fa33604c5611353ce77
baojun <bj805887485@aliyun.com>
2018-01-21 13:39:27 +0800

    no message

--- server/mongodb/updateAllMovies.js
+++ server/mongodb/updateAllMovies.js
@@ -29,7 +29,7 @@ function update(type, callback) {
             if (type == 0) {
                 number = 1
             } else {
-                number = 3578
+                number = 8005
             }
             get(length, number, function () {
                 callback(1)
