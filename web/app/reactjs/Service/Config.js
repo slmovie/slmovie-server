b52e81e55c8e34ef88a2d9794dfa0dcaa9efcd5d
baojun <bj805887485@aliyun.com>
2018-03-16 15:08:21 +0800

    no message

--- web/app/reactjs/Service/Config.js
+++ web/app/reactjs/Service/Config.js
@@ -7,7 +7,7 @@ exports.WebRoot = function () {
     if (Constans.service == 'T') {
         return 'http://localhost:3000'
     } else if (Constans.service == 'P') {
-        return 'http://www.slys.cf'
+        return '45.32.41.169:3000'
     }
 }
 
