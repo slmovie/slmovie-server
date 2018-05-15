039225191dca09ef97664dd9365a1f15e7384bf0
baojun <bj805887485@aliyun.com>
2018-05-15 14:51:22 +0800

    豆瓣id爬取报错修复

--- server/mongodb/updateAllMovies.js
+++ server/mongodb/updateAllMovies.js
@@ -26,9 +26,9 @@ function update(type, callback) {
             if (type == 0) {
                 number = 1
             } else {
-                // number = 0
+                number = 0
                 // number = length - 2000
-                number = 10000
+                // number = 10000
             }
             get2(length, number, function () {
                 callback(1)
