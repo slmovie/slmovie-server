220145f9fded1985d8098986c9c439022e7603ff
baojun <bj805887485@aliyun.com>
2018-05-17 19:50:16 +0800

    增加客户端更新功能

--- rn/version/detail.js
+++ rn/version/detail.js
@@ -2,5 +2,5 @@
  * Created by 包俊 on 2017/8/15.
  */
 exports.Version = "1.0.0"
-exports.APPVersion = "1.1"
-exports.APPUpdateInfo="是点击开发阶段疯狂拉升的到来"
+exports.APPVersion = "1.0"
+exports.APPUpdateInfo=""
