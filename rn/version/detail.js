ff592c533418e4827db23995e9d88598825524a0
baojun <bj805887485@aliyun.com>
2018-07-04 17:20:19 +0800

    客户端更新1.4

--- rn/version/detail.js
+++ rn/version/detail.js
@@ -2,5 +2,5 @@
  * Created by 包俊 on 2017/8/15.
  */
 exports.Version = "1.0.1"
-exports.APPVersion = "1.3"
-exports.APPUpdateInfo = "可以直接在app中下载啦，无需使用其他下载工具，在线播放正在紧张开发中"
+exports.APPVersion = "1.4"
+exports.APPUpdateInfo = "可在app中下载与在线播放"
