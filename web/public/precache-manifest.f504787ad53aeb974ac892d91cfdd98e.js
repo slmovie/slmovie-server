2362fbae7714432145fd5fbfe1d01cd8067a82f8
秋月夜 <baojun.cz@gmail.com>
2019-01-16 16:13:48 +0800

    feat: update new version web

---
+++ web/public/precache-manifest.f504787ad53aeb974ac892d91cfdd98e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "06ac892a2d554a68e280",
+    "url": "/static/js/main.06ac892a.chunk.js"
+  },
+  {
+    "revision": "49740e1b0493c3caed08",
+    "url": "/static/js/1.49740e1b.chunk.js"
+  },
+  {
+    "revision": "06ac892a2d554a68e280",
+    "url": "/static/css/main.a40af95a.chunk.css"
+  },
+  {
+    "revision": "49740e1b0493c3caed08",
+    "url": "/static/css/1.62f061f5.chunk.css"
+  },
+  {
+    "revision": "36ee612d1c6d5ad01d676489b95d5d6b",
+    "url": "/index.html"
+  }
+];
