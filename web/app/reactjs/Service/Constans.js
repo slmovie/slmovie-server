31476e7e5c69906128bb04eae78d11e7eb6d297b
秋月夜 <baojun.cz@gmail.com>
2019-01-16 19:33:56 +0800

    chore: remove useless code

--- web/app/reactjs/Service/Constans.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by BaoJun on 2017/2/23.
- */
-//测试地址T，生产地址P
-exports.service = 'P'
-exports.log = false
-exports.error = false
--- web/app/reactjs/Service/HtmlCode.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by BaoJun on 2017/2/23.
- */
-import Config from './Config.js'
-
-exports.Index = Config.WebRoot() + '/index.html'
-exports.SearchResult = Config.WebRoot() + '/searchresult.html'
-exports.Detail = Config.WebRoot() + '/detail.html'
