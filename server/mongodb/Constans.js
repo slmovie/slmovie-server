87e3f42d6df9e448e333f6b77abf9cda781279f8
秋月夜 <baojun.cz@gmail.com>
2019-03-12 19:52:33 +0800

    chore: rollback,make repo private

--- server/mongodb/Constans.js
+++ server/mongodb/Constans.js
@@ -6,8 +6,8 @@ let service = "P";
 
 exports.WebRoot = function () {
   if (service === "T") {
-    return "Your mongodb address";
+    return "mongodb://baojunMovies:sadsdagds45+6412gds@localhost:14369";
   } else if (service === "P") {
-    return "Your mongodb address";
+    return "mongodb://baojunMovies:sadsdagds45+6412gds@45.32.41.169:14369";
   }
 };
